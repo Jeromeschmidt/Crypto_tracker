aa5d4a5a498857e39c7ee06dea76f7472d829b62
Jerome <jerome.schmidt@students.makeschool.com>
2021-03-04 10:47:18 -0600

    update to time

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Crypto Prices Tracker
 
 [![Go Report Card](https://goreportcard.com/badge/github.com/Jeromeschmidt/Crypto_tracker)](https://goreportcard.com/report/github.com/Jeromeschmidt/Crypto_tracker)
+
+Used Colly to scrape Crypto prices from Coinmarketcap.com and send notifications to user at desired intervals with updates.
--- main.go
+++ main.go
@@ -8,22 +8,8 @@ import (
 	"encoding/json"
 	"os"
 	"github.com/gocolly/colly"
-    // twilio "github.com/kevinburke/twilio-go"
+    twilio "github.com/kevinburke/twilio-go"
 )
-
-// var thCsrf string
-//
-// type Assignments struct {
-//     Users []Assignment `json:"assignments"`
-// }
-//
-// type Assignment struct {
-// 	Title	string
-// 	Link	string
-// 	Class string
-//     DueDate string
-//     Reminded bool
-// }
 
 // type Coins struct {
 // 	Coins []Coin `json:"Coins"`
@@ -51,16 +37,13 @@ func run(){
 		msg, _ := client.Messages.SendMessage("+18787898352", "+14028407963", "Daily Crypto prices have been updated", nil)
 		fmt.Println(msg.Sid, msg.FriendlyPrice())
 		// 86400
-        time.Sleep(10 * time.Second)
+        time.Sleep(28800 * time.Second)
     }
 }
 
 func scrape() {
-		type Coins struct {
-			Coins []Coin `json:"Coins"`
-		}
 
-        c := colly.NewCollector()
+		c := colly.NewCollector()
 
         c.OnHTML("tr", func(e *colly.HTMLElement) {
     		name, price, dayChange := "", "", ""
@@ -86,8 +69,6 @@ func scrape() {
                 }
             })
 
-			fmt.Println(time.Now().String()[1])
-
 
             coin := Coin{
     			Name:       name,
@@ -111,7 +92,6 @@ func scrape() {
 
 func writeFile(coin Coin){
 	JSONcoin, _ := json.MarshalIndent(coin, "", " ")
-	// fmt.Println(string(JSONcoin))
 
 	f, err := os.OpenFile("coins.json", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0660)
 	if err != nil {
@@ -124,79 +104,3 @@ func writeFile(coin Coin){
 		panic(err)
 	}
 }
-
-// func main() {
-// 	// create a new collector
-// 	c := colly.NewCollector(
-//         // colly.AllowedDomains("gradescope.com", "https://www.gradescope.com"),
-//     )
-//     //
-//     // client := twilio.NewClient("ACaf356b30e339a6e9d0dba1f51aa4d989", "284305ff72993b5ab89fb22732ed3f1d", nil)
-//     //
-//     // msg, _ := client.Messages.SendMessage("+18787898352", "+14028407963", "Sent via go :) ✓", nil)
-//     // fmt.Println(msg.Sid, msg.FriendlyPrice())
-//
-//
-//     // authenticity_token
-//     // csrf-token
-//     // extract TH_CSRF token for the session
-// 	// c.OnHTML("form[role=form] input[type=hidden][name=csrf-token]", func(e *colly.HTMLElement) {
-// 	// 	thCsrf := e.Attr("value")
-// 	// 	log.Println(thCsrf)
-// 	// 	return
-//     // })
-//     c.OnHTML("[name=csrf-token]", func(e *colly.HTMLElement) {
-// 		thCsrf := e.Attr("content")
-// 		log.Println(thCsrf)
-// 		return
-//     })
-//
-// 	// authenticate
-// 	err := c.Post("https://www.gradescope.com/login", map[string]string{"session_email": "jerome.schmidt@students.makeschool.com", "session_password": "Jms1014*neb123", "csrf-token": thCsrf})
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-//     // On every a element which has href attribute call callback
-// 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-//         log.Println(e.Text)
-//         link := e.Attr("href")
-//         if !strings.Contains(link, "/courses/") {
-//             return
-//         }
-//
-//
-//         // if !strings.HasPrefix(link, "/courses/") {
-//         //     return
-//         // }
-//         // if e.Attr("class") == "courseBox" {
-// 		// /	return
-//     // })
-//     //
-//         // e.Request.Visit(link)
-//         log.Println(link)
-//         c.Visit(e.Request.AbsoluteURL(link))
-//     //     // link := e.Attr("href")
-// 	// 	// // If link start with browse or includes either signup or login return from callback
-// 	// 	// if !strings.HasPrefix(link, "/browse") || strings.Index(link, "=signup") > -1 || strings.Index(link, "=login") > -1 {
-// 	// 	// 	return
-// 	// 	// }
-// 	// 	// // start scaping the page under the link found
-// 	// 	// e.Request.Visit(link)
-//     //
-//     })
-//
-//     // Before making a request print "Visiting ..."
-// 	c.OnRequest(func(r *colly.Request) {
-// 		log.Println("visiting", r.URL.String())
-// 	})
-//
-// 	// attach callbacks after login
-// 	c.OnResponse(func(r *colly.Response) {
-// 		log.Println("response received", r.StatusCode)
-//         c.Visit("https://www.gradescope.com/courses/233222")
-// 	})
-//
-// 	// start scraping
-// 	c.Visit("https://www.gradescope.com/login")
-// }
